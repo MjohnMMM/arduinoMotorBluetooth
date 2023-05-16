47bc5fc7c3b3a0fe5738808bc7869c47d4124ae5
MjohnMMM <43796478+MjohnMMM@users.noreply.github.com>
2023-05-16 22:03:20 +0200

    Dhá tionscadal uaslóidáilt
    Dhá tionscadal a bheadh úsáideach chun an gluaistean/breágan a rith

---
+++ Fernsteuerung_Spielzeug_Projekt_rev2/Fernsteuerung_Spielzeug_Projekt_rev2.ino
@@ -0,0 +1,133 @@
+// es gibt keine Schalterumsetzung in diesem Code
+const int X_pin = A0; // analog pin connected to X output
+const int Y_pin = A1; // analog pin connected to Y output
+
+const int capacSens_1 = 2;
+const int capacSens_2 = 3;
+
+int touch_1=0;
+int touch_2=0;
+
+int x_pin_reading=0;
+int y_pin_reading=0;
+
+int left_right=0;
+int forward_backward=0;
+bool left_active;
+bool right_active;
+bool forward_active;
+bool backward_active;
+int padding=30;
+int JoystickFactor=1;
+int boost=0;
+
+int vel=0;
+
+
+void setup() {
+
+  pinMode(capacSens_1,INPUT);
+  pinMode(capacSens_2,INPUT);
+  Serial.begin(19200);
+
+  
+}
+
+void loop() {
+
+  touch_1=digitalRead(capacSens_1);
+  touch_2=digitalRead(capacSens_2);
+  x_pin_reading=analogRead(X_pin);
+  y_pin_reading=analogRead(Y_pin);
+  
+
+  if(x_pin_reading<(328-padding))
+  {
+    right_active=false;
+    left_active=true;
+    left_right=(328-x_pin_reading)*JoystickFactor;
+    Serial.print("left=");
+    Serial.println(left_right);
+  }
+
+  if(x_pin_reading>(328+padding))
+  {
+    left_active=false;
+    right_active=true;
+    left_right=(328-x_pin_reading)*JoystickFactor;
+    Serial.print("right = ");
+    Serial.println(left_right);
+  }
+
+  if(y_pin_reading>(330+padding))
+  {
+    backward_active=false;
+    forward_active=true;
+    forward_backward=(330-y_pin_reading)*JoystickFactor;
+    Serial.print("foward = ");
+    Serial.println(forward_backward);
+    boost=boost-1;
+  }
+
+  if(y_pin_reading<(330-padding))
+  {
+    forward_active=false;
+    backward_active=true;    
+    forward_backward=(330-y_pin_reading)*JoystickFactor;
+    Serial.print("backward = ");
+    Serial.println(forward_backward);
+    boost=boost+1;
+  }
+
+  if(y_pin_reading>(330-padding) && y_pin_reading<(330+padding))
+  {
+    if(boost<0)
+    {
+      boost = boost + 1;
+    }
+    else if(boost > 0)
+    {
+     boost = boost - 1; 
+    }
+    else
+    {
+      boost=0;
+    }
+  }
+
+  if(boost>5){
+    boost=5;
+  }else if(boost < -5){
+    boost=-5;
+  }
+
+
+  if(touch_1==1 && vel !=5)
+  {
+    vel=vel+1;
+    //Serial.println("capacSens pad 2 is HIGH");
+  }
+  
+
+  if(touch_2==1 && vel != -5)
+  {
+    vel=vel-1;
+  }
+
+  
+   
+  
+  if(right_active==true)
+  {
+    Serial.println("turning left");
+  }
+  else if(left_active== true)
+  {
+    Serial.println("turning right");
+  }
+  
+  Serial.print("total velocity = ");
+  Serial.println(vel + boost);
+  
+  delay(500);
+}
---
+++ MotorKontrolleRev5-Bluetooth/MotorKontrolleRev5-Bluetooth.ino
@@ -0,0 +1,101 @@
+
+int terminalA = 12;
+int terminalB = 13;
+
+int speedA = 3;
+int speedB = 11;
+
+int brakeA = 9;
+int brakeB = 8;
+
+int velocity=50;
+
+const int ledPin = 13;//the led attach to
+char val[4];
+
+
+void setup() {
+  //Schritt 1: Setze die Rictung (Polaritaet) des Motor
+  pinMode(terminalA, OUTPUT);
+  pinMode(terminalB, OUTPUT);
+  
+  //Schritt 2: Loese (disengage) die Bremse
+  pinMode(brakeA, OUTPUT);
+  digitalWrite(brakeA, LOW);
+
+  pinMode(brakeB, OUTPUT);
+  digitalWrite(brakeB, LOW);
+
+  pinMode(ledPin,OUTPUT);//initialize the ledPin as an output
+
+  //Schritt 3: Setze die Geschwindigkeit (PWM Signal)
+  
+  // Zusaetzliche Initialisierungsdingen
+  Serial.begin(9600); // zu auf dem Serial Fenster Potentiometerwerte ausdrucken
+
+ 
+}
+
+void loop() {
+
+      if( Serial.available() )       // if data is available to read
+     {
+      for(int i=0; i<4;i++)
+      {
+       val[i] = Serial.read();         // read it and store it in 'val'
+       Serial.print("I received: ");
+       Serial.println(val[i]);
+      }
+       
+     }
+     if( val[0] == '1' ) digitalWrite(ledPin, HIGH);  // turn ON the LED
+     if( val[0] == '2' ) digitalWrite(ledPin, HIGH);  // turn ON the LED
+
+    // Seite A : Ruckwaerts
+    if(val[0] == '1'){
+      digitalWrite(terminalA, LOW);    
+      analogWrite(speedA, velocity);
+      stopMotor("B");
+    }else if (val[0] == '2'){
+      // Seite A : Vorwaerts
+      digitalWrite(terminalA, HIGH);
+      analogWrite(speedA, -velocity);
+      stopMotor("B");
+    }else if (val[0] == '0'){    
+      // Seite A: Halte
+      digitalWrite(terminalA, HIGH);
+      analogWrite(speedA, 0);
+      stopMotor("B");
+    }else if (val[3] == '1'){
+      // Seite B: Ruckwaerts
+      digitalWrite(terminalB, LOW);
+      analogWrite(speedB, velocity);
+      stopMotor("A");
+    }else if (val[0] == '2'){
+      // Seite B : Vorwaerts
+      digitalWrite(terminalB, HIGH);
+      analogWrite(speedB, -velocity);
+      stopMotor("A");
+    }else if (val[0] == '0'){
+      // Seite B: Halte
+      analogWrite(speedB, 0);
+      stopMotor("A");
+    }
+    
+  delay(400);
+  
+}
+
+void stopMotor(char motor)
+{
+  if(motor=="A")
+  {
+    analogWrite(speedA, 0);
+  }else if(motor=="B"){
+    analogWrite(speedB, 0);
+  }
+  
+}
+
+
+
