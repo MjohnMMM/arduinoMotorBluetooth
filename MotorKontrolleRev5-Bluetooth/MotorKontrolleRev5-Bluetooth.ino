d61d9f91ceedec8527e8ce8bc0fc95ed073b0eb7
LenovoMiixJohn <johnmannion9@gmail.com>
2023-06-06 21:22:49 +0200

    new update. Motors working.

--- MotorKontrolleRev5-Bluetooth/MotorKontrolleRev5-Bluetooth.ino
+++ MotorKontrolleRev5-Bluetooth/MotorKontrolleRev5-Bluetooth.ino
@@ -8,7 +8,7 @@ int speedB = 11;
 int brakeA = 9;
 int brakeB = 8;
 
-int velocity=50;
+int velocity=150;
 
 const int ledPin = 13;//the led attach to
 char val[4];
@@ -36,53 +36,72 @@ void setup() {
  
 }
 
+void test_loop()
+{
+  digitalWrite(terminalA, LOW);    
+  analogWrite(speedA, velocity*3);
+  digitalWrite(terminalA, LOW);    
+  analogWrite(speedA, velocity*3);
+}
+
 void loop() {
 
+    
       if( Serial.available() )       // if data is available to read
      {
-      for(int i=0; i<4;i++)
-      {
-       val[i] = Serial.read();         // read it and store it in 'val'
+      //for(int i=0; i<4;i++)
+      //{
+       val[0] = Serial.read();         // read it and store it in 'val'
        Serial.print("I received: ");
-       Serial.println(val[i]);
-      }
+       Serial.println(val[0]);
+     // }
        
      }
+
+      //digitalWrite(terminalA, LOW);    
+      //analogWrite(speedA, velocity*3);
+
+
      if( val[0] == '1' ) digitalWrite(ledPin, HIGH);  // turn ON the LED
      if( val[0] == '2' ) digitalWrite(ledPin, HIGH);  // turn ON the LED
 
-    // Seite A : Ruckwaerts
-    if(val[0] == '1'){
-      digitalWrite(terminalA, LOW);    
-      analogWrite(speedA, velocity);
-      stopMotor("B");
-    }else if (val[0] == '2'){
-      // Seite A : Vorwaerts
-      digitalWrite(terminalA, HIGH);
-      analogWrite(speedA, -velocity);
-      stopMotor("B");
-    }else if (val[0] == '0'){    
-      // Seite A: Halte
-      digitalWrite(terminalA, HIGH);
-      analogWrite(speedA, 0);
-      stopMotor("B");
-    }else if (val[3] == '1'){
-      // Seite B: Ruckwaerts
-      digitalWrite(terminalB, LOW);
-      analogWrite(speedB, velocity);
-      stopMotor("A");
-    }else if (val[0] == '2'){
-      // Seite B : Vorwaerts
-      digitalWrite(terminalB, HIGH);
-      analogWrite(speedB, -velocity);
-      stopMotor("A");
-    }else if (val[0] == '0'){
-      // Seite B: Halte
-      analogWrite(speedB, 0);
-      stopMotor("A");
+    switch(val[0])
+    {
+      case '1':
+        digitalWrite(terminalA, LOW);    
+        analogWrite(speedA, velocity);
+      break;
+
+      case '2':
+        digitalWrite(terminalA, HIGH);
+        analogWrite(speedA, -velocity);
+      break;
+
+      case '3':
+        digitalWrite(terminalB, LOW);
+        analogWrite(speedB, velocity);
+      break;
+
+      case '4':
+        digitalWrite(terminalB, HIGH);
+        analogWrite(speedB, -velocity);
+      break;
+
+      case '5':
+        analogWrite(speedA, 0);
+      break;
+
+      case '6':
+        analogWrite(speedB, 0);
+      break;
+
+      default:
+
+      break;
+
     }
     
-  delay(400);
+  delay(100);
   
 }
 
